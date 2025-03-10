1346db6f16f0c99300820650f3833b25b980d9aa
nazaropr <90557490+nazaropr@users.noreply.github.com>
2025-03-10 16:29:25 +0200

    hooks useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
 
 
 const App = () => {
-  return (
+    let [counter, setCounter] = React.useState<number>(0);
+    let [x, setX] = React.useState<number>(0);
+
+    useEffect(() => {
+        setTimeout(()=>{
+            setCounter(counter + 1);
+        },2000)
+        return () => {console.log('.........useEffect callback')}
+    }, [x]);
+console.log('.');
+
+    return (
       <div>
-        
+
+          <h2>{counter}</h2>
+          <button onClick={()=>{
+              setX(x=> x+1);
+          }}>click</button>
       </div>
   );
 };
