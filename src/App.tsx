af2603293017406fc62dd1e8349ecf687cd13e2a
nazaropr <90557490+nazaropr@users.noreply.github.com>
2025-03-20 15:24:00 +0200

    useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React, {FC, memo, useCallback, useState} from 'react';
+import React, {FC, memo, useCallback, useMemo, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
-// в компоненту передається функція, при кліку на кнопку не буде спрацьовувати мемо бо кожен раз передається новий об'єкт
+//ситуація як з useCallback тільки тут ми передаємо об'єкт
 const A:FC<any> = memo(({foobar}) => {
     console.log('Hello from A component');
     foobar();
@@ -17,18 +16,19 @@ const App = () => {
   console.log('Hello from App component');
   let [count, setCount] = useState(0);
 
-  // при кліку на кнопку в нас встановлюється нове значення в count відповідно компонента перередерюється і так само перестворюється констента функції foobar тобто
-  //   абсолютно новий об'єкт з новою коміркою в пам'яті компонента A бачить що в пропсу заходить новий об'єкт і відповідно робить рендеринг по новій
-
-    // const foobar = () =>{
-  //
-  // }
   const foobar = useCallback(()=>{
 
   }, []);
+
+  //створює стале посилання для об'єкта щоб не перемальовувалась компонента A, в deps ([]) можна передати змінну за якою
+  //   буде слідкувати useMemo і при зміні, робити рендеринг по новій
+  const arr = useMemo(() => {
+      return [];
+  },[])
+
     return (
       <div>
-        <A foobar={foobar}></A>
+        <A foobar={foobar} arr={arr}></A>
           <button onClick={()=>{
               setCount(count + 1);
           }}>render</button>
