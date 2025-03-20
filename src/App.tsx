735663448901bfbfe844dd3d123e2efdd6720bcd
nazaropr <90557490+nazaropr@users.noreply.github.com>
2025-03-20 14:12:08 +0200

    memo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {memo, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 
+// memo() зупиняє ре-рендеринг компоненти, яку обгорнуто в нього
+const A = memo(() => {
+    console.log('Hello from A component');
+    return (
+        <div>hello from A component</div>
+    )
+})
+
+
 const App = () => {
-  return (
+  console.log('Hello from App component');
+  let [count, setCount] = useState(0);
+    return (
       <div>
-        
+        <A></A>
+          <button onClick={()=>{
+              setCount(count + 1);
+          }}>render</button>
       </div>
   );
 };
