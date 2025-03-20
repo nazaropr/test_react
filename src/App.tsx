d933c3e7846a6e7cfc034cf7af3d68d0e0047e28
nazaropr <90557490+nazaropr@users.noreply.github.com>
2025-03-20 20:37:58 +0200

    controlled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, {FormEvent, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface IFormProps {
+    username: string;
+    password: string;
+}
 
 const App = () => {
-  return (
+  let [formSate, setFormSate] = useState<IFormProps>({
+      username: '',
+      password: ''
+  })
+
+    const handleChange = (e:FormEvent<HTMLInputElement>) => {
+        let input = e.currentTarget;
+        // console.log(input.value);
+        setFormSate({...formSate, [input.name]: input.value});
+    };
+
+    const handleSubmit = (e:FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        let user = {
+            username: formSate.username,
+            password: formSate.password,
+        }
+        console.log(user)
+    }
+
+    return (
       <div>
-        
+          <form onSubmit={handleSubmit}>
+              <input type="text" name={'username'} value={formSate.username} onChange={handleChange}/>
+              <input type="text" name={'password'} value={formSate.password} onChange={handleChange}/>
+              <button>submit</button>
+          </form>
       </div>
   );
 };
