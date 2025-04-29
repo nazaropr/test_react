aeb5197dbfec379ddae09fcdb9b1ebe6ab2554ba
nazaropr <90557490+nazaropr@users.noreply.github.com>
2025-04-29 20:06:57 +0300

    redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { decrement, increment, useAppDispatch, useAppSelector} from "./redux/store";
 
 
 const App = () => {
-  return (
+
+    const {value} = useAppSelector(store => store.slice1);
+    
+    const dispatch = useAppDispatch();
+    return (
       <div>
-        
+          <h1>{value}</h1>
+          <button onClick={()=> {
+            dispatch(increment(100));
+          }}>increment</button>
+          <button onClick={()=> {
+            dispatch(decrement())
+          }}
+          >decrement</button>
       </div>
   );
 };
